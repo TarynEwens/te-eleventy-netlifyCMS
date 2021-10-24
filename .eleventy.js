aea4705a7a00697efa563d8c1bd9d17e283b5067
tarynewens <taryn@taryn.codes>
2021-10-24 19:37:01 +1100

    basic blog set up

---
+++ .eleventy.js
@@ -0,0 +1,19 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("admin");
+
+  const { DateTime } = require("luxon");
+
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter("htmlDateString", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {
+      zone: "utc",
+    }).toFormat("yy-MM-dd");
+  });
+
+  eleventyConfig.addFilter("readableDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {
+      zone: "utc",
+    }).toFormat("dd-MM-yy");
+  });
+};
